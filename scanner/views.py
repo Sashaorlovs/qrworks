6ae3ede44296b26f2a614b19592be523424f02b4
Sashaorlovs <sashaorlov.m@gmail.com>
2026-06-02 18:30:22 +0300

    Дата отгрузки перенесена на страницу заказа, с главной удалена, курсив исправлен

--- scanner/views.py
+++ scanner/views.py
@@ -1309,3 +1309,19 @@ def change_order_status(request, order_id, new_status):
     order.save()
     messages.success(request, f'Статус заказа изменён на {order.get_status_display()}.')
     return redirect('order_detail', order_id=order.id)
+
+@login_required
+def update_order_due_date(request, order_id):
+    if not request.user.is_staff:
+        messages.error(request, 'Недостаточно прав.')
+        return redirect('home')
+    order = get_object_or_404(Order, pk=order_id)
+    if request.method == 'POST':
+        due_date = request.POST.get('due_date')
+        if due_date:
+            order.due_date = due_date
+        else:
+            order.due_date = None
+        order.save()
+        messages.success(request, f'Дата отгрузки заказа {order.order_number} обновлена.')
+    return redirect('home')
